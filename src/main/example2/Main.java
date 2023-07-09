04ff8b5f26b99bd1fdc54e07f943f6b865927dbf
Byron Katz <byronka@msn.com>
2023-07-09 17:16:39 -0400

    update framework, adjust main

--- src/main/example2/Main.java
+++ src/main/example2/Main.java
@@ -1,6 +1,8 @@
 package example2;
 
+import minum.Constants;
 import minum.FullSystem;
+import minum.logging.Logger;
 import minum.web.StartLine;
 
 import minum.web.Response;
@@ -8,13 +10,28 @@ import minum.web.WebFramework;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try (WebFramework wf = FullSystem.initialize()) {
-            // register a handler
-            wf.registerPath(
-                    StartLine.Verb.GET,
-                    "",
-                    request -> Response.htmlOk("<p>Hi there world!</p>"));
-        }
+    public static void main(String[] args) throws Exception {
+        // Initialize the constants, these are used throughout the system
+        var constants = new Constants();
+
+        // Build the logger.  We separate the construction of the logger
+        // as a separate task to avoid circular dependencies.  The logger
+        // uses a lot of the same objects as FullSystem.
+        var logger = Logger.make(constants);
+
+        // Start the system
+        FullSystem fs = FullSystem.initialize(logger, constants);
+
+        // Register some endpoints
+        fs.getWebFramework().registerPath(
+                StartLine.Verb.GET,
+                "",
+                request -> Response.htmlOk("<p>Hi there world!</p>"));
+
+        // These are here to block, as a backstop, since we are dealing with a
+        // multithreaded system.  Without these, the program would
+        // simply pass right on and exit.
+        fs.getServer().centralLoopFuture.get();
+        fs.getSslServer().centralLoopFuture.get();
     }
 }
