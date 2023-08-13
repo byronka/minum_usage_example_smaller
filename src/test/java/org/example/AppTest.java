3d0e4d16e37140bbcec7493869165bdce8efad9c
Byron Katz <byronka@msn.com>
2023-08-13 16:54:18 -0400

    Remove junit - can use tests prefixed with "test-" instead

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,14 +3,12 @@ package org.example;
 import static minum.testing.TestFramework.assertTrue;
 
 import minum.web.FullSystem;
-import org.junit.Test;
 
 
 public class AppTest
 {
 
-    @Test
-    public void shouldAnswerWithTrue()
+    public void testBasics()
     {
         assertTrue( true );
     }
