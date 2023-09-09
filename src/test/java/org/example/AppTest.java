417d3e22758a14f90161b86bd2af635d66651b8e
Byron Katz <byronka@msn.com>
2023-09-09 17:22:26 -0400

    Add junit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,11 +3,13 @@ package org.example;
 import static com.renomad.minum.testing.TestFramework.assertTrue;
 
 import com.renomad.minum.web.FullSystem;
+import org.junit.Test;
 
 
 public class AppTest
 {
 
+    @Test
     public void testBasics()
     {
         assertTrue( true );
