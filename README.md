90ab584087ee818dc56d6621023515a835007cdf
Byron Katz <byronka@msn.com>
2025-01-14 20:16:26 -0500

    Adding a bit more capability
    More demonstrated features:
    
    - using the Response class to redirect
    - serving static content: an HTML page and an image
    - using forms
    - slightly more sophisticated business logic
    - show pattern to use StringUtils.encode() to mitigate JavaScript execution with input

--- README.md
+++ README.md
@@ -2,27 +2,7 @@ Minum example - small
 ====================
 
 This is a small project meant to demonstrate use of the [Minum framework](https://github.com/byronka/minum).
-Check out the [code](src/main/java/org/example/Main.java). 
-
-Here it is:
-
-```java
-public class Main {
-
-   public static void main(String[] args) {
-      // Start the system
-      FullSystem fs = FullSystem.initialize();
-
-      // Register some endpoints
-      fs.getWebFramework().registerPath(
-              RequestLine.Method.GET,
-              "",
-              request -> Response.htmlOk("<p>Hi there world!</p>"));
-
-      fs.block();
-   }
-}
-```
+Check out the [code](src/main/java/org/example/Main.java).
 
 Quick start:
 ------------
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.renomad.minum.utils.StringUtils;
 import com.renomad.minum.web.FullSystem;
 import com.renomad.minum.web.Response;
 
@@ -9,7 +10,21 @@ public class Main {
 
     public static void main(String[] args) {
         FullSystem fs = FullSystem.initialize();
-        fs.getWebFramework().registerPath(GET, "", request -> Response.htmlOk("<p>Hi there world!</p>"));
+
+        fs.getWebFramework().registerPath(GET, "", request -> Response.redirectTo("/index.html"));
+
+        fs.getWebFramework().registerPath(GET, "hello", request -> {
+            String name = request.getRequestLine().queryString().get("name");
+            if (name == null || name.isBlank()) {
+                return Response.htmlOk("<p>Hi there world!</p>");
+            } else {
+                // encoding prevents XSS attacks
+                String urlEncodedName = StringUtils.encode(name);
+                return Response.htmlOk("<p>Hi there "+urlEncodedName+"!</p>");
+            }
+
+        });
+
         fs.block();
     }
 }
