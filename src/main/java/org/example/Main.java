abd8e9b2cc5699fea8904fd47e75ceeb7da5d4f4
Byron Katz <byronka@msn.com>
2025-11-07 19:25:42 -0500

    Refactors, example tests and better documentation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,21 @@ import static com.renomad.minum.web.RequestLine.Method.GET;
 public class Main {
 
     public static void main(String[] args) {
+        // Start the application
         FullSystem fs = FullSystem.initialize();
 
+        // Register the endpoints
+        registerEndpoints(fs);
+
+        fs.block();
+    }
+
+    /**
+     * This method handles registering the endpoints that this
+     * application will serve.  By splitting it out to its own
+     * method, the testing becomes much easier.  See AppTest.java
+     */
+    public static void registerEndpoints(FullSystem fs) {
         fs.getWebFramework().registerPath(GET, "", request -> Response.redirectTo("/index.html"));
 
         fs.getWebFramework().registerPath(GET, "hello", request -> {
@@ -24,7 +37,5 @@ public class Main {
             }
 
         });
-
-        fs.block();
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,17 +1,123 @@
 package org.example;
 
-import static com.renomad.minum.testing.TestFramework.assertTrue;
+import com.renomad.minum.logging.TestLogger;
+import com.renomad.minum.state.Constants;
+import com.renomad.minum.state.Context;
+import com.renomad.minum.web.FullSystem;
+import com.renomad.minum.web.FunctionalTesting;
+import com.renomad.minum.web.StatusLine;
+import org.junit.*;
 
-import org.junit.Test;
+import static com.renomad.minum.testing.TestFramework.*;
+import static com.renomad.minum.web.StatusLine.StatusCode.CODE_200_OK;
 
 
 public class AppTest
 {
 
+    private Context context;
+    private TestLogger logger;
+    private FullSystem fullSystem;
+    private Constants constants;
+    private FunctionalTesting ft;
+
+    /**
+     * Initialize a full system startup for each test
+     */
+    @Before
+    public void init() {
+        this.context = buildTestingContext("AppTest");
+        this.constants = this.context.getConstants();
+        this.logger = (TestLogger) context.getLogger();
+
+        this.fullSystem = new FullSystem(context);
+        this.fullSystem.start();
+
+        Main.registerEndpoints(this.fullSystem);
+
+        this.ft = new FunctionalTesting(context, this.constants.hostName, this.constants.serverPort);
+    }
+
+    /**
+     * Cleanly shut down the system after each test
+     */
+    @After
+    public void cleanup() {
+        this.fullSystem.shutdown();
+        this.context.getLogger().stop();
+        this.context.getExecutorService().shutdownNow();
+    }
+
+    /**
+     * Test getting a static resource, like "moon.png" which comes bundled with
+     * this project in src/main/webapp/static/moon.webp
+     * <br>
+     * Adjust the location of your static values by modifying the setting
+     * in minum.config, which defaults to src/main/webapp/static.
+     * <br>
+     * When using Minum, it is expected that the local development configuration
+     * may be different than for other environments like testing or prod.
+     */
     @Test
-    public void testBasics()
+    public void testGettingStaticResource()
     {
-        assertTrue( true );
+        // execute an HTTP GET request for moon.webp
+        FunctionalTesting.TestResponse testResponse = ft.get("moon.webp");
+
+        assertEquals(testResponse.statusLine().status(), CODE_200_OK);
+        assertEquals(testResponse.body().asBytes().length, 4190);
+    }
+
+    /**
+     * If we send a GET request to the "hello" endpoint without a query
+     * string, it is expected to respond with "Hi there world" in HTML.
+     */
+    @Test
+    public void testHelloEndpoint_Default() {
+        // execute an HTTP GET request for the "hello" endpoint
+        FunctionalTesting.TestResponse testResponse = ft.get("hello");
+
+        assertEquals(testResponse.statusLine().status(), CODE_200_OK);
+        assertEquals(testResponse.body().asString(), "<p>Hi there world!</p>");
+    }
+
+    /**
+     * Similar to {@link #testHelloEndpoint_Default()} but he we
+     * specify the name in the query string
+     */
+    @Test
+    public void testHelloEndpoint_SpecificNameValue() {
+        // execute an HTTP GET request for the "hello" endpoint
+        FunctionalTesting.TestResponse testResponse = ft.get("hello?name=alice");
+
+        assertEquals(testResponse.statusLine().status(), CODE_200_OK);
+        assertEquals(testResponse.body().asString(), "<p>Hi there alice!</p>");
+    }
+
+    /**
+     * Confirm we get what is expected for the index.html page, the
+     * page presented to the user when they hit http://localhost:8080/index.html
+     */
+    @Test
+    public void testIndexHappyPath() {
+        // execute an HTTP GET request for the "index" endpoint
+        FunctionalTesting.TestResponse testResponse = ft.get("index.html");
+
+        assertEquals(testResponse.statusLine().status(), CODE_200_OK);
+        assertEquals(testResponse.body().asString().length(), 239);
+
+    }
+
+    /**
+     * If the user requests http://localhost:8080/, the system will
+     * redirect their browser to index.html
+     */
+    @Test
+    public void testIndexRedirect() {
+        FunctionalTesting.TestResponse testResponseEmpty = ft.get("");
+
+        assertEquals(testResponseEmpty.statusLine().status(), StatusLine.StatusCode.CODE_303_SEE_OTHER);
+        assertEquals(testResponseEmpty.headers().valueByKey("location").getFirst(), "/index.html");
     }
 
 
